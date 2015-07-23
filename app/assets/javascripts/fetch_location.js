22f86b0c7cc7083a9bbe4179fc5be015989631d7
Lia Skalkos <hello@liaskalkos.net>
2015-07-23 17:21:55 -0400

    Slight refactor of fetch_location.js

--- app/assets/javascripts/fetch_location.js
+++ app/assets/javascripts/fetch_location.js
@@ -1,45 +1,51 @@
-$(document).ready(function(){
-
-  var dots;
-
-  getLocation();
-  setInterval(addDots, 250);
-
-  $("form").submit(function(event){
-    event.preventDefault();
-    var data = $(this).serializeArray();
-    var method = $(this).attr("method"); 
-    var url = $(this).attr("action");
-    $.ajax({
-      method: method,
-      url: url,
-      data: data,
-      dataType: "script"
-      }); 
-    });
-
-});
-
 function addDots(){
-  if(dots < 3){
-    $("#dots").append('.');
-      dots++;
+    if(dots < 3){
+        $("#dots").append('.');
+        dots++;
     }
-  else{
-    $("#dots").empty();
-      dots = 0;
+    else{
+        $("#dots").empty();
+        dots = 0;
     }
 };
 
 function getLocation() {
-  var longitude, latitude;
+    var longitude, latitude;
 
-  navigator.geolocation.getCurrentPosition(function(position){
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    $("#brewery_latitude").val(eval(latitude));
-    $("#brewery_longitude").val(eval(longitude));
-    $("#beer-form").show();
-    $("h3").hide();
-  });
+    navigator.geolocation.getCurrentPosition(function(position){
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        $("#brewery_latitude").val(eval(latitude));
+        $("#brewery_longitude").val(eval(longitude));
+        $("#beer-form").show();
+        $("h3").hide();
+    });
 }
+
+function loadPage(){
+    var dots;
+    setInterval(addDots, 250);
+}
+
+function setFormListener(){
+    $("form").submit(function(event){
+        event.preventDefault();
+        var data = $(this).serializeArray();
+        var method = $(this).attr("method");
+        var url = $(this).attr("action");
+        $.ajax({
+            method: method,
+            url: url,
+            data: data,
+            dataType: "script"
+        });
+    });
+}
+
+
+$(function(){
+    getLocation();
+    loadPage();
+    setFormListener();
+});
+
