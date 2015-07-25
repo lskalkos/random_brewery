49f7cef0da73e636166b5c1d7904509632b22325
Lia Skalkos <hello@liaskalkos.net>
2015-07-24 20:25:12 -0400

    Refactor google_map.js

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,26 +1,26 @@
 
 function GoogleMap(){
 
-    var directionsService = new google.maps.DirectionsService();
     var map;
-    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
 
-    this.initialize = function(lat, lng) {
-        directionsDisplay = new google.maps.DirectionsRenderer();
-        var start_location = new google.maps.LatLng(lat, lng);
-        var mapOptions = {
-            zoom:13,
-            center: start_location
-        };
-        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-        directionsDisplay.setMap(map);
+    this.start_location = null,
+    this.mapOptions = {
+        zoom: 13,
+        center: this.start_location
     }
 
+    this.addMaptoPage = function(){
+        map = new google.maps.Map(document.getElementById('map-canvas'), this.mapOptions);
+        directionsDisplay.setMap(map);
+    },
+
     this.calcRoute = function(lat_end, long_end) {
-        var start = new google.maps.LatLng(start_lat, start_lng);
+
         var end = new google.maps.LatLng(lat_end, long_end);
         var request = {
-            origin:start,
+            origin: this.start_location,
             destination:end,
             travelMode: google.maps.TravelMode.WALKING
         };
@@ -30,7 +30,13 @@ function GoogleMap(){
             }
         });
     }
+
 }
+
+GoogleMap.prototype.initialize = function(lat, lng) {
+    this.start_location = new google.maps.LatLng(lat, lng);
+}
+
 
 
 
